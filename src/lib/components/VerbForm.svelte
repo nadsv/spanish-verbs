b65a8649318f0af773bf6313da5411e57b0c7c6e
Veranona <nadsvezh@yandex.ru>
2024-04-19 13:20:51 +0300

    keydown event added

--- src/lib/components/VerbForm.svelte
+++ src/lib/components/VerbForm.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { Input, Label, Button } from 'flowbite-svelte';
 	import { conjunctions }  from './helpers';
 	import { createEventDispatcher } from 'svelte';
@@ -14,11 +14,17 @@
 		});
 	}
 
+	function keyDown(event: KeyboardEvent) {
+		if (event.key === 'Enter') {
+			findConjunctions();
+		}
+	}
+
 </script>
 
 <div class="flex w-full gap-x-3 items-center">
 	<Label for="first_name" class="mb-2">Infinitive</Label>
-	<Input type="text" id="infinitive" bind:value={verb} required />
+	<Input type="text" id="infinitive" bind:value={verb} required on:keydown={keyDown}/>
 	<Button class="bg-primary-500" on:click={findConjunctions}>FIND</Button>
 </div>
 
