0fa486287534dbbb739c3e3c2ebd56fc62e69066
Veranona <nadsvezh@yandex.ru>
2024-04-23 10:05:23 +0300

    add style for small screens in result table

--- src/lib/components/ResultTable.svelte
+++ src/lib/components/ResultTable.svelte
@@ -4,7 +4,8 @@
 	export let cnj;
 </script>
 
-<Table striped={true} shadow class="wrapper">
+	
+<Table striped={true} shadow >
 	<TableBody>
 		<TableBodyRow  class="!bg-primary-600">
 				<TableBodyCell class="uppercase text-center text-white px-3 py-2">tense</TableBodyCell>
@@ -14,9 +15,9 @@
 		</TableBodyRow>
 		{#each Object.entries(cnj) as [tense, persons]}
 			<TableBodyRow>
-				<TableBodyCell class="bg-primary-200 px-3 py-1">{tenseName(tense)}</TableBodyCell>
+				<TableBodyCell class="bg-primary-200 px-3 py-1"><span class="font-small">{tenseName(tense)}</span></TableBodyCell>
 				{#each Object.entries(persons) as [number, name]}
-					<TableBodyCell class="text-center px-3 py-1">{name}</TableBodyCell>
+					<TableBodyCell class="text-center px-3 py-1"><span class="font-small">{name}</span></TableBodyCell>
 				{/each}
 			</TableBodyRow>
 		{/each}
@@ -25,4 +26,9 @@
 
 
 <style>
+@media (max-width: 1200px) { 
+	.font-small {
+		font-size: 11px !important;
+	}
+}	
 </style>
